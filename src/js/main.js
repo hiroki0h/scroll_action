5a20f1befbf1af977530b8f1bff99563e72871e7
재연 <jjyeon@betree.co.kr>
2020-11-30 18:21:01 +0900

    스크롤 비율 계산

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 //(function(){
     let yOffset = 0; // 현재 스크롤 높이 저장
     let currentScene = 0; // 현재 활성화 된 씬
+    let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 있는 섹션들 높이 합
+    let sceneChange = false; // 씬이 변경될 때 활성화
     
     const sceneInfo = [
         {
@@ -18,7 +20,7 @@ import $ from 'jquery';
             },
             values: {
                 messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
-                messageB_opacity_in: [0, 1, { start: 0.4, end: 0.5 }],
+                messageA_opacity_out: [1, 0, { start: 0.25, end: 0.35 }],
             }
         }
     ];
@@ -33,29 +35,69 @@ import $ from 'jquery';
         
         // 현재 스크롤높이
         yOffset = window.pageYOffset;
-        console.log(yOffset);
+        console.log('yOffset',yOffset);
     };
     
     // 4
-    const calcValues = () => {        
+    const calcValues = (values, currentYOffset) => {  
+        let returnValue;  
         const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        
+        // 모션 시작 끝 구간
+        const partScrollStart = values[2].start * scrollHeight;
+        const partScrollEnd = values[2].end * scrollHeight;
+        // 모션 높이
+        const partScrollHeight = partScrollEnd - partScrollStart;
+        
+        
         // 씬 처음과 끝을 비율로 만들기
         // 내가 보고 있는 구간 
+        console.log('values',values);
+        console.log('currentYOffset',currentYOffset);
+        
+        returnValue = 
         
     };
     
     // 3    
     const playAnimation = () => {
+        
+        const objs = sceneInfo[currentScene].objs;  
+        const values = sceneInfo[currentScene].values;  
+        
+        // 현재 씬에서의 스크롤 높이
+        const currentYOffset = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight; // 현재 씬에서 스크롤 높이만큼의 비율
+        
+        
+//        console.log('currentYOffset',currentYOffset);
+//        console.log('prevScrollHeight',prevScrollHeight);
+        
+        if( scrollRatio <= 0.22 ){ // 모션 들어갔다
+            objs.messageA.css({
+                'opacity': calcValues(values.messageA_opacity_in, currentYOffset),
+                'transform': `translate3d(${calcValues(values.messageA_translateX_in, currentYOffset)}%, -50%, 0)`
+            });
+        }else{
+            objs.messageA.css({ // 모션 나오기
+                'opacity': calcValues(values.messageA_opacity_out, currentYOffset),
+                'transform': `translate3d(${calcValues(values.messageA_translateX_out, currentYOffset)}%, -50%, 0)`
+            });
+        }
     };
-    
+        
+    const scrollLoop = () => {
+        playAnimation();
+    };
+        
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
+        scrollLoop();
     });
     
     window.addEventListener('load', () => {
         setLayout();
-        
-        playAnimation();
 //        console.log(sceneInfo[0]);
     });
     
